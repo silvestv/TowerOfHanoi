857fc6e96158f38e2f4b19abff48fc4f977e4dec
Victor <victor.silvestre0@gmail.com>
2019-05-05 21:01:15 +0200

    ajout des fonctionnnalité succés-erreur erreur-succés du timer/refonte timer entre les actions/restructuration/comment

--- towerOfHanoi/src/main/java/org/sadok/TowerOfHanoi/Timer.java
+++ towerOfHanoi/src/main/java/org/sadok/TowerOfHanoi/Timer.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class Timer {
     private long t_second_game_start;
     private long t_second_game_stop;
-    private long t_second_game__total;
+    private long t_second_game_total;
 
     private long triggerSuccess;
     private long triggerError;
@@ -16,21 +16,32 @@ public class Timer {
     private long t_second_between_action;
     private long t_second_between_error;
     private long t_second_between_success;
+    private long t_second_between_success_then_error;
+    private long t_second_between_error_then_sucess;
 
     private ArrayList<Long> allBetweenSucess;
     private ArrayList<Long> allBetweenError;
     private ArrayList<Long> allBetweenAction;
+    private ArrayList<Long> allBetweenSuccessThenError;
+    private ArrayList<Long> allBetweenErrorThenSucess;
 
     private long t_average_second_action;
     private long t_average_second_error;
     private long t_average_second_success;
+    private long t_average_second_sucess_then_error;
+    private long t_average_second_error_then_sucess;
 
+    ///////////CONSTRUCT
     public Timer(){
         this.allBetweenSucess = new ArrayList<>();
         this.allBetweenError = new ArrayList<>();
         this.allBetweenAction = new ArrayList<>();
+        this.allBetweenSuccessThenError = new ArrayList<>();
+        this.allBetweenErrorThenSucess = new ArrayList<>();
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //CHRONO GENERAL
     //permet de lancer le chrono
     public void start(){
         this.t_second_game_start = System.currentTimeMillis();
@@ -47,33 +58,57 @@ public class Timer {
         return t_second_game_start - t_second_game_stop;
     }
 
-    //déclenche le timer entre 2 succès --> si aucun trigger n'a été initialiser, on l'initialise, sinon,
-    //on prend en compte le temps entre le 1er et le 2nd succès et on range ce temps dans un vecteur AllBetweenSucess
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //LES TRIGGERS (calcul des différences entre action, succès, erreurs, succès-erreur, erreur-succès
+
+    //déclenche en cas d'action de succès
+    //déclenche le timer entre 2 succès ou 1 erreur puis succès--> si aucun trigger n'a été initialiser, on l'initialise, sinon,
+    //on prend en compte le temps entre le 1er et le 2nd succès et on range ce temps dans un vecteur AllBetweenSucess ou allBetweenErrorThenSucess
     public void clickSuccess(){
+        //init
         if(triggerSuccess == 0){
             this.triggerSuccess = System.currentTimeMillis();
+
+        //entre 2 succès
         } else {
             this.t_second_between_success = System.currentTimeMillis() - this.triggerSuccess;
-            this.triggerSuccess = System.currentTimeMillis();
             this.allBetweenSucess.add(t_second_between_success);
 
+            //entre echec puis succès
+            if(triggerError != 0){
+                this.t_second_between_error_then_sucess = System.currentTimeMillis() - this.triggerError;
+                this.allBetweenErrorThenSucess.add(t_second_between_error_then_sucess);
+            }
+            this.triggerSuccess = System.currentTimeMillis();
         }
     }
 
+    //déclenche en cas d'action d'erreurs
     //même fonctionnement pour les erreurs
     public void clickError(){
+        //init
         if(triggerError == 0){
             this.triggerError = System.currentTimeMillis();
+
+        //entre 2 echec
         } else {
             this.t_second_between_error = System.currentTimeMillis() - this.triggerError;
-            this.triggerError = System.currentTimeMillis();
             this.allBetweenError.add(t_second_between_error);
+
+            //entre succès puis erreur
+            if(triggerSuccess != 0){
+                this.t_second_between_success_then_error = System.currentTimeMillis() - this.triggerSuccess;
+                this.allBetweenSuccessThenError.add(t_second_between_success_then_error);
+            }
+            this.triggerError = System.currentTimeMillis();
         }
     }
 
     //même fonctionnement entre chaque action du joueur
     public void clickAction(){
         if(triggerAction == 0){
+            //entre la permière action et le debut de la partie
+            this.t_second_between_action = System.currentTimeMillis() - this.t_second_game_start;
             this.triggerAction = System.currentTimeMillis();
         } else {
             this.t_second_between_action = System.currentTimeMillis() - this.triggerAction;
@@ -81,6 +116,9 @@ public class Timer {
             this.allBetweenAction.add(t_second_between_action);
         }
     }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////CALCUL DES TEMPS MOYENS AU COURS D'UNE PARTIE
 
     //en fin de game, on récupère le vecteur indiquant tout les lapses-time entre les actions et on en retire la moyenne
     public void calculAverageTimeAction(){
@@ -98,7 +136,7 @@ public class Timer {
         for(int i = 0; i <= allBetweenSucess.size(); i++){
             sum = sum + allBetweenSucess.get(i);
         }
-        this.t_average_second_success = sum/allBetweenAction.size();
+        this.t_average_second_success = sum/allBetweenSucess.size();
     }
 
     //fin de game, même fonctionnement que pour le AverageTimeAction()
@@ -107,10 +145,30 @@ public class Timer {
         for(int i = 0; i <= allBetweenError.size(); i++){
             sum = sum + allBetweenError.get(i);
         }
-        this.t_average_second_success = sum/allBetweenAction.size();
+        this.t_average_second_error = sum/allBetweenError.size();
     }
 
+    public void calculAverageTimeSucessThenError(){
+        long sum = 0;
+        for(int i = 0; i <= allBetweenSuccessThenError.size(); i++){
+            sum = sum + allBetweenSuccessThenError.get(i);
+        }
+        this.t_average_second_sucess_then_error = sum/allBetweenSuccessThenError.size();
+    }
+
+    public void calculAverageTimeErrorThenSucess(){
+        long sum = 0;
+        for(int i = 0; i <= allBetweenErrorThenSucess.size(); i++){
+            sum = sum + allBetweenErrorThenSucess.get(i);
+        }
+        this.t_average_second_error = sum/allBetweenErrorThenSucess.size();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////LES ACCESSEURS
+
     //Les getteurs
+
     public ArrayList<Long> getAllBetweenAction(){
         return this.allBetweenAction;
     }
@@ -123,6 +181,16 @@ public class Timer {
         return this.allBetweenError;
     }
 
+    public ArrayList<Long> getAllBetweenSucessThenError(){
+        return this.allBetweenSuccessThenError;
+    }
+
+    public ArrayList<Long> getAllBetweenErrorThenSucess(){
+        return this.allBetweenErrorThenSucess;
+    }
+
+
+
     public long getAverageTimeAction(){
         return this.t_average_second_action;
     }
@@ -133,6 +201,15 @@ public class Timer {
     public long getAverageTimeError(){
         return this.t_average_second_error;
     }
+
+    public long getAverageTimeSucessThenError(){
+        return this.t_average_second_sucess_then_error;
+    }
+    public long getAverageTimeErrorThenSucess(){
+        return this.t_average_second_error_then_sucess;
+    }
+
+
 
     public long getTimeBetweenAction(){
         return this.t_second_between_action;
@@ -145,6 +222,15 @@ public class Timer {
         return this.t_second_between_error;
     }
 
+    public long getTimeBetweenSuccessThenError(){
+        return this.t_second_between_success_then_error;
+    }
+    public long getTimeBetweenErrorThenSucess(){
+        return this.t_second_between_error_then_sucess;
+    }
+
+
+
     public long getTriggerSuccess(){
         return this.triggerSuccess;
     }
@@ -156,6 +242,8 @@ public class Timer {
         return this.triggerAction;
     }
 
+
+
     public long getT_second_game_start(){
         return t_second_game_start;
     }
@@ -163,7 +251,5 @@ public class Timer {
     public long getT_second_game_stop(){
         return t_second_game_stop;
     }
-
-
 
 }
